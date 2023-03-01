e63cf1836d8a47cff20ed47f3f1b641df2822fe1
Aleksey Karelin <64694787+Wolferado@users.noreply.github.com>
2023-03-01 15:35:11 +0200

    Small change to put branch ahead

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using ArtificialIntelligenceGraphChallenge;
 
+// Autora informācijas izvade
 Console.WriteLine("Autors: Aleksejs Kareļins 211RDB141 \n");
 
+// Grafa izveide
 Graph graph = new Graph();
 
+// Grafa informācijas izvade
 graph.PrintOutAllStatuses();
 graph.PrintOutInformationalStatuses();
 graph.PrintOutTheGraph();
